e7f9ca9b8d1b476dc1f71087cba6afe74a3dac8b
Andy Newhouse <andy@techenby.com>
2025-11-17 09:30:56 -0600

    Add flux to styles and layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>{{ $title ?? config('app.name') }}</title>
+
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=inter:400,500,600&display=swap" rel="stylesheet" />
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    @fluxAppearance
+</head>
+<body>
+{{ $slot }}
+
+@fluxScripts
+</body>
+</html>
