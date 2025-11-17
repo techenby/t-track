7c39a4d8f193714710608df5e32ebe9d9969b3d6
Andy Newhouse <andy@techenby.com>
2025-11-17 13:59:30 -0600

    Add errors and back button

--- resources/views/livewire/pages/create-log.blade.php
+++ resources/views/livewire/pages/create-log.blade.php
@@ -4,13 +4,19 @@
         <div>
             <flux:button icon="microphone" wire:click="recordVoice"></flux:button>
         </div>
+
+        <flux:error name="voice" />
     </flux:field>
     <flux:field>
         <flux:label>Take Photo</flux:label>
         <div>
             <flux:button icon="camera" wire:click="takePhoto"></flux:button>
         </div>
+        <flux:error name="image" />
     </flux:field>
 
-    <flux:button variant="primary" wire:click="save">Save</flux:button>
+    <div class="flex justify-between items-center">
+        <flux:button :href="route('logs')" icon="chevron-left">Cancel</flux:button>
+        <flux:button variant="primary" wire:click="save">Save</flux:button>
+    </div>
 </form>
